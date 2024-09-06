ea76889175a65c01ac54745b4eb7ec9519042e88
Jormanji <alacritydrummer@hotmail.co.uk>
2024-09-06 16:47:51 +0100

    Add rendering of movie card and trending movies

--- src/app/MovieCard.js
+++ src/app/MovieCard.js
@@ -1,3 +1,18 @@
-export default function MovieCard(){
-    
-}
+import React from "react";
+
+export default function MovieCard({ movie }) {
+    return (
+      <div className="movie-card">
+        <img 
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+          alt={movie.title} 
+          className="movie-card__image"
+        />
+        <div className="movie-card__details">
+          <h3 className="movie-card__title">{movie.title}</h3>
+          <p className="movie-card__rating">Rating: {movie.vote_average}</p>
+          <p className="movie-card__release-date">Release Date: {movie.release_date}</p>
+        </div>
+      </div>
+    );
+  }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import styles from "./page.module.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import CategoryDisplay from "./CategoryDisplay";
+import TrendingMovies from "./TrendingMovies";
 
 export default function Home() {
   return (
     <div>
     <Header />
-    <CategoryDisplay />
+    <TrendingMovies />
     <Footer />
     </div>
   );
