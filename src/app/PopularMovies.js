e1202c2558d0dd64406dc4beddef42a36b1a88c3
Jormanji <alacritydrummer@hotmail.co.uk>
2024-09-08 15:00:44 +0100

    Add styling changes across the whole landing page

--- src/app/PopularMovies.js
+++ src/app/PopularMovies.js
@@ -12,7 +12,8 @@ export default function PopularMovies () {
     useEffect(() => {
         fetchPopularMovies()
         .then((response) => {
-            setMovies(response.data.results)
+            const firstFiveMovies = response.data.results.slice(0, 5);
+            setMovies(firstFiveMovies)
             setLoading(false)
         })
         .catch((error) => {
